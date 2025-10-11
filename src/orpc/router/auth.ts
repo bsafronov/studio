822127f14133add50e5b86d0aa79e044b758caab
Bogdan Safronov <safronovba@modeltech.ru>
2025-10-12 02:10:32 +0300

    add auth

---
+++ src/orpc/router/auth.ts
@@ -0,0 +1,76 @@
+import { usersTable } from "@/db/schema";
+import { createSession, generateSessionToken } from "@/lib/auth";
+import { ORPCError } from "@orpc/server";
+import { setCookie } from "@tanstack/react-start/server";
+import bcrypt from "bcryptjs";
+import { eq } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-zod";
+import { baseProcedure } from "../utils";
+
+
+
+const RegisterSchema = createInsertSchema(usersTable).pick({
+  username: true,
+  password: true,
+});
+ 
+const register = baseProcedure.input(RegisterSchema).handler(async ({input, context: {db}}) => {
+  const existingUser = await db.query.usersTable.findFirst({
+    where: eq(usersTable.username, input.username)
+  })
+
+  if (existingUser) {
+    throw new ORPCError('CONFLICT', {
+      message: 'User already exists',
+    });
+  }
+
+  const hashPassword = await bcrypt.hash(input.password, 10);
+
+  const [user] = await db.insert(usersTable).values({
+    username: input.username,
+    password: hashPassword
+  }).returning();
+
+  const token = generateSessionToken();
+
+  await createSession(token, user.id);
+
+  setCookie('sessionToken', token, {
+    httpOnly: true
+  })
+})
+
+const LoginSchema = RegisterSchema
+const login = baseProcedure.input(LoginSchema).handler(async ({context, input}) => {
+  const user = await context.db.query.usersTable.findFirst({
+    where: eq(usersTable.username, input.username)
+  });
+
+  if (!user) {
+    throw new ORPCError('UNAUTHORIZED', {
+      message: 'Invalid credentials',
+    });
+  }
+
+  const isValidPassword = await bcrypt.compare(input.password, user.password);
+
+  if (!isValidPassword) {
+    throw new ORPCError('UNAUTHORIZED', {
+      message: 'Invalid credentials',
+    });
+  }
+
+  const token = generateSessionToken();
+
+  await createSession(token, user.id);
+
+  setCookie('sessionToken', token, {
+    httpOnly: true
+  })
+})
+
+export const auth = {
+  register,
+  login
+}
