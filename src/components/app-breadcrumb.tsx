47e3af9661708e326c07b1f26548c986965133d3
Bogdan Safronov <safronovba@modeltech.ru>
2025-10-19 20:02:29 +0300

    add generic breadcrumb

--- src/components/app-breadcrumb.tsx
+++ src/components/app-breadcrumb.tsx
@@ -1,9 +1,55 @@
+import { Link, useLocation } from "@tanstack/react-router";
+import { Fragment } from "react/jsx-runtime";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbSeparator,
+} from "./ui/breadcrumb";
 import { SidebarTrigger } from "./ui/sidebar";
 
+const PATH_DICTIONARY: Record<string, string> = {
+	sheets: "Таблицы",
+	columns: "Столбцы",
+	rows: "Строки",
+	new: "Новая запись",
+	auth: "Авторизация",
+	login: "Вход",
+	register: "Регистрация",
+};
+
 export function AppBreadcrumb() {
+	const location = useLocation();
+	const paths = location.pathname.split("/").slice(1);
+
 	return (
-		<div className="p-4 border-b">
+		<div className="p-2 border-b flex items-center gap-2">
 			<SidebarTrigger />
+			<Breadcrumb>
+				<BreadcrumbList>
+					{paths.map((item, idx) => {
+						const path = `/${paths.slice(0, idx + 1).join("/")}`;
+
+						return (
+							<Fragment key={idx.toString()}>
+								<BreadcrumbItem>
+									<BreadcrumbLink asChild>
+										<Link
+											to={path}
+											className="data-[status='active']:text-blue-500 data-[status='active']:hover:text-blue-400"
+											activeOptions={{ exact: true }}
+										>
+											{PATH_DICTIONARY[item] ?? item}
+										</Link>
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+								{idx !== paths.length - 1 && <BreadcrumbSeparator />}
+							</Fragment>
+						);
+					})}
+				</BreadcrumbList>
+			</Breadcrumb>
 		</div>
 	);
 }
